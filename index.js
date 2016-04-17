4fc6615ebba3c3165bc4ee2f32ff36f4cedfaeb8
Rumata <Rumatadeestor@gmail.com>
2016-04-17 20:59:39 +0300

    1 res added.

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var app = express();
 app.get('/', (req, res, next) => {
   res.send('Hello World');
 });
+app.get('/user/:id', (req, res, next) => {
+  res.send(`${req.params.id} user page.`);
+});
 
 app.listen(process.env.PORT || 8080, ()=> {
   console.log('Started...');
