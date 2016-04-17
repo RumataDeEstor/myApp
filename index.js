7209da938f8b33155229af85ede434f0666d4c1c
Rumata <Rumatadeestor@gmail.com>
2016-04-17 21:35:35 +0300

    right pages

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var app = express();
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-app.get('/', (req, res, next) => {
-  res.render('index.html');
-});
-app.get('/user/:id', (req, res, next) => {
-  res.send(`${req.params.id} user page.`);
-});
+app.use('/user/0', function (req, res, next) {
+	res.render('index.html');
+}) 
+app.use('/user/1', function (req, res, next) {
+	res.send("Да это же AJAX :)");
+}) 
 
 app.listen(process.env.PORT || 8080, ()=> {
   console.log('Started...');
